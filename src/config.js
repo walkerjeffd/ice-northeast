285190fb3ad4386cea80424df01d4d708eeb9858
Jeff Walker <walker.jeff.d@gmail.com>
2019-03-29 11:17:02 -0400

    add aggregation img, update config

--- src/config.js
+++ src/config.js
@@ -2,13 +2,11 @@ export default {
   development: {
     api: {
       url: 'http://localhost:8081/data/'
-      // url: 'http://s3.amazonaws.com/ecosheds.org/ice-sheds/'
     }
   },
   production: {
     api: {
-      // url: 'http://s3.amazonaws.com/ecosheds.org/ice-sheds/'
-      url: 'http://localhost:8081/data/'
+      url: 'https://ecosheds.org.s3.amazonaws.com/ice-sheds/'
     }
   }
 }
